b9d211d0460900b34532037fa5177b0c521ab8f6
SourabTN <mdnhs.cse@gmail.com>
2024-03-29 01:57:47 +0600

    navbar classname update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,18 +6,18 @@ type Props = {};
 const NavBar = (props: Props) => {
   return (
     <div className=" bg-transparent fixed z-10 hover:bg-white h-20  content-center w-full grid grid-cols-3 px-12 ">
-      <div className="flex gap-3 justify-start">
+      <div className="flex items-center gap-3 justify-start">
         <p className=" cursor-pointer">Menu</p>
         <div className=" cursor-pointer">Search</div>
       </div>
-      <div className="flex justify-center">
+      <div className="flex items-center justify-center">
         <Link href={"/"}>
           <p className="text-3xl font-bold tracking-widest cursor-pointer">
             LOUIS VUITTON
           </p>
         </Link>
       </div>
-      <div className="flex gap-3 justify-end">
+      <div className="flex items-center gap-3 justify-end">
         <p className=" cursor-pointer">Call Us</p>
         <p className=" cursor-pointer">Wishlist</p>
         <p className=" cursor-pointer">user</p>
