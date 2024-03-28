da93f6413217dae425b570589a2ce4bfb63711f2
SourabTN <mdnhs.cse@gmail.com>
2024-03-29 01:15:01 +0600

    NavBar Font Size Update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,14 +5,14 @@ type Props = {};
 
 const NavBar = (props: Props) => {
   return (
-    <div className=" bg-transparent shadow-md hover:bg-white h-20 w-full flex justify-between px-12 items-center">
+    <div className=" bg-transparent fixed z-10 hover:bg-white h-20 w-full flex justify-between px-12 items-center">
       <div className="flex gap-3">
         <p className=" cursor-pointer">Menu</p>
         <div className=" cursor-pointer">Search</div>
       </div>
       <div>
         <Link href={"/"}>
-          <p className="text-xl font-bold tracking-widest cursor-pointer">
+          <p className="text-3xl font-bold tracking-widest cursor-pointer">
             LOUIS VUITTON
           </p>
         </Link>
