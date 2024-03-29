63e65faf094698f1365b029cc21d8de1f4864cc0
SourabTN <mdnhs.cse@gmail.com>
2024-03-29 12:51:46 +0600

    NavBar Scrolling Color Change Feature Added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,31 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
 const NavBar = (props: Props) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className=" bg-transparent fixed z-10 hover:bg-white h-20  content-center w-full grid grid-cols-3 px-12 ">
+    <div
+      className={`${
+        scrollPosition > 80 ? "bg-white" : "bg-transparent hover:bg-white"
+      }  fixed z-10  h-20  content-center w-full grid grid-cols-3 px-12 `}
+    >
       <div className="flex items-center gap-3 justify-start">
         <p className=" cursor-pointer">Menu</p>
         <div className=" cursor-pointer">Search</div>
