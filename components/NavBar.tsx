1b0da8bc35a2401d35e381e0cce7df9a2aaef2da
SourabTN <mdnhs.cse@gmail.com>
2024-03-29 20:18:24 +0600

    Nested Drawer Done

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { AlignJustify, Search, ShoppingBag } from "lucide-react";
+import { Search, ShoppingBag } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import CallUsDrawer from "./CallUsDrawer";
 import LoginDrawer from "./LoginDrawer";
+import MenuDrawer from "./MenuDrawer";
 
 type Props = {};
 
@@ -33,9 +34,7 @@ const NavBar = (props: Props) => {
       }  fixed z-10  h-20  content-center w-full grid grid-cols-3 px-12 `}
     >
       <div className="flex items-center gap-6 justify-start">
-        <div className=" cursor-pointer flex gap-2 items-center">
-          <AlignJustify size={20} strokeWidth={1.5} /> <p>Menu</p>
-        </div>
+        <MenuDrawer/>
         <div className=" cursor-pointer flex gap-2 items-center">
           <Search size={20} strokeWidth={1.5} />
           <p>Search</p>
