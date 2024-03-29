5642d87d3647a3efcb5ba21d1a4c714ac155cdd0
SourabTN <mdnhs.cse@gmail.com>
2024-03-29 13:23:15 +0600

    NavBar Icon Added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AlignJustify, Search, ShoppingBag, User } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -6,7 +7,7 @@ type Props = {};
 
 const NavBar = (props: Props) => {
   const [scrollPosition, setScrollPosition] = useState(0);
-  
+
   useEffect(() => {
     const handleScroll = () => {
       const position = window.scrollY;
@@ -26,9 +27,14 @@ const NavBar = (props: Props) => {
         scrollPosition > 80 ? "bg-white" : "bg-transparent hover:bg-white"
       }  fixed z-10  h-20  content-center w-full grid grid-cols-3 px-12 `}
     >
-      <div className="flex items-center gap-3 justify-start">
-        <p className=" cursor-pointer">Menu</p>
-        <div className=" cursor-pointer">Search</div>
+      <div className="flex items-center gap-6 justify-start">
+        <div className=" cursor-pointer flex gap-2 items-center">
+          <AlignJustify size={20} strokeWidth={1.5} /> <p>Menu</p>
+        </div>
+        <div className=" cursor-pointer flex gap-2 items-center">
+          <Search size={20} strokeWidth={1.5} />
+          <p>Search</p>
+        </div>
       </div>
       <div className="flex items-center justify-center">
         <Link href={"/"}>
@@ -37,11 +43,18 @@ const NavBar = (props: Props) => {
           </p>
         </Link>
       </div>
-      <div className="flex items-center gap-3 justify-end">
-        <p className=" cursor-pointer">Call Us</p>
+      <div className="flex items-center gap-6 justify-end">
+        <div className=" cursor-pointer">Call Us</div>
         <p className=" cursor-pointer">Wishlist</p>
-        <p className=" cursor-pointer">user</p>
-        <p className=" cursor-pointer">cart</p>
+        <div className=" cursor-pointer">
+          <User size={20} strokeWidth={1.5} />
+        </div>
+        <div className=" cursor-pointer relative ">
+          <ShoppingBag size={20} strokeWidth={1.5} />
+          <p className="bg-black w-3.5 h-3.5 absolute -top-2 -right-4 rounded-full text-white text-center text-[10px] flex justify-center">
+            0
+          </p>
+        </div>
       </div>
     </div>
   );
