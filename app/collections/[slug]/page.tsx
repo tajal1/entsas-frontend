95c4513773c67994839f43d86dd136e8a325eecb
SourabTN <mdnhs.cse@gmail.com>
2024-03-30 23:48:42 +0600

    product details done

--- app/collections/[slug]/page.tsx
+++ app/collections/[slug]/page.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const page = ({ params: { slug } }: any) => {
   return (
-    <div className=" bg-gray-100 px-12">
+    <div className=" bg-gray-100">
       {/* <strong>{slug}</strong> */}
       <ProductDetails />
     </div>
