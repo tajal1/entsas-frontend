91eda342f4bd66f0e0f89cda26861034fb2898a9
ahsan-ahmed-rakib <ahsan.raib@technonext.com>
2024-03-30 14:18:52 +0600

    Modal added for wishlist

--- app/wishList/page.tsx
+++ app/wishList/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>WishList</div>;
+  return <div className="py-16">WishList</div>;
 };
 
 export default page;
--- components/collections/CollectionCard.tsx
+++ components/collections/CollectionCard.tsx
@@ -5,10 +5,23 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 import { useAppStore } from "@/service/store";
-import { Heart } from "lucide-react";
+import { Heart, X } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 type Props = { mediaList: any; title: string; currency: string; price: number };
 
@@ -22,50 +35,66 @@ const CollectionCard = (item: Props) => {
     setGlobalIsLove(isLove);
   }, [isLove]);
 
-  console.log(globalIsLove);
-  console.log(isLove);
+  console.log("global is love", globalIsLove);
+  console.log("is love", isLove);
 
   return (
-    <div className=" space-y-3 overflow-hidden gap-4 h-80 w-full relative">
-      <Heart
-        onClick={() => setIsLove((prevIsLove) => !prevIsLove)}
-        size={20}
-        fill={isLove ? "black" : "transparent"}
-        strokeWidth={1.5}
-        className=" absolute right-3 top-5 z-10 cursor-pointer"
-      />
-      <Carousel
-        opts={{
-          align: "start",
-          loop: true,
-        }}
-      >
-        <CarouselContent className="group">
-          {item.mediaList.map((media: any, mediaIdx: number) => {
-            return (
-              <CarouselItem
-                className=" bg-gradient-to-tl from-gray-100 via-gray-100 to-gray-200 flex justify-center items-center relative w-full h-60 "
-                key={mediaIdx}
-              >
-                <Image
-                  src={media.src}
-                  alt="media"
-                  fill
-                  className="object-contain h-60 w-full absolute"
-                />
-              </CarouselItem>
-            );
-          })}
-        </CarouselContent>
-        <CarouselPrevious className="left-5 bg-transparent border-none " />
-        <CarouselNext className="right-5 bg-transparent border-none " />
-      </Carousel>
+    <>
+      <AlertDialog open={globalIsLove} onOpenChange={setGlobalIsLove}>
+        <AlertDialogContent className="flex justify-between">
+          <AlertDialogDescription className="text-black">
+            This Item has been added to your wishlist <br />{" "}
+            <span className="underline">
+              <Link href="/wishlist">Access Your Wishlist</Link>
+            </span>
+          </AlertDialogDescription>
+          <AlertDialogCancel className="border-none">
+            <X />
+          </AlertDialogCancel>
+        </AlertDialogContent>
+      </AlertDialog>
 
-      <p>{item.title}</p>
-      <p>
-        <span>{item.currency}</span> {item.price.toLocaleString()}
-      </p>
-    </div>
+      <div className=" space-y-3 overflow-hidden gap-4 h-80 w-full relative">
+        <Heart
+          onClick={() => setIsLove((prevIsLove) => !prevIsLove)}
+          size={20}
+          fill={isLove ? "black" : "transparent"}
+          strokeWidth={1.5}
+          className=" absolute right-3 top-5 z-10 cursor-pointer"
+        />
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+        >
+          <CarouselContent className="group">
+            {item.mediaList.map((media: any, mediaIdx: number) => {
+              return (
+                <CarouselItem
+                  className=" bg-gradient-to-tl from-gray-100 via-gray-100 to-gray-200 flex justify-center items-center relative w-full h-60 "
+                  key={mediaIdx}
+                >
+                  <Image
+                    src={media.src}
+                    alt="media"
+                    fill
+                    className="object-contain h-60 w-full absolute"
+                  />
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          <CarouselPrevious className="left-5 bg-transparent border-none " />
+          <CarouselNext className="right-5 bg-transparent border-none " />
+        </Carousel>
+
+        <p>{item.title}</p>
+        <p>
+          <span>{item.currency}</span> {item.price.toLocaleString()}
+        </p>
+      </div>
+    </>
   );
 };
 
