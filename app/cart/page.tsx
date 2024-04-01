790718cea384465a8991c1993b3c898fd637dc6d
SourabTN <mdnhs.cse@gmail.com>
2024-04-01 23:36:14 +0600

    cart page ui 50% done

---
+++ app/cart/page.tsx
@@ -0,0 +1,14 @@
+import CartContainer from "@/components/cartPage/CartContainer";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className=" bg-gray-100">
+      <CartContainer />
+    </div>
+  );
+};
+
+export default page;
