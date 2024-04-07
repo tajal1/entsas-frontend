8a9695752303ea770567240d5f19be2585d25320
SourabTN <mdnhs.cse@gmail.com>
2024-04-08 01:19:36 +0600

    image optimize

--- components/landingPage/LandingPageCard.tsx
+++ components/landingPage/LandingPageCard.tsx
@@ -11,7 +11,7 @@ type LandingPageCardType = {
 
 const LandingPageCard = ({ img, title, url, urlText }: LandingPageCardType) => {
   return (
-    <Link href={url ?? "/"} className="w-full h-[calc(100vh-200px)]">
+    <Link href={url ?? "/"} className="w-full h-[calc(100vh-500px)] lg:h-[calc(100vh-200px)]">
       <div className="w-full h-full md:h-80 xl:h-full cursor-pointer space-y-3">
         <div className="relative w-full h-[85%]">
           <Image src={img} alt="Image" fill={true} className=" object-cover" />
